4d3d2a738c936d6ca38b2e9e0e989d2b54b36f50
AlissonKawan <rm567598@fiap.com.br>
2025-09-02 23:57:59 -0300

    commit

---
+++ moeda.py
@@ -0,0 +1,5 @@
+d = int(input('Digite quantos dolares voce tem: '))
+
+real = (d * 6)
+
+print(f'Voce tem R${real} ')
---
+++ temperatura.py
@@ -0,0 +1,5 @@
+Celsius = float(input("Digite a temperatura em Celsius: "))
+
+Fahrenheit = (Celsius * 9/5) + 32
+
+print(f"A temperatura em Fahrenheit é: {Fahrenheit}f")
