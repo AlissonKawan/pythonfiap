a51157fde9f3d4bb2a1100fdbbcf3d5490d34007
AlissonKawan <rm567598@fiap.com.br>
2025-09-04 22:44:15 -0300

    commit

--- Produtos.py
+++ Produtos.py
@@ -1,15 +1,15 @@
-prodt1 =  int(input('Digite o valor do produto 1: '))
-prodt2 =  int(input('Digite o valor do produto 2: '))
-prodt3 =  int(input('Digite o valor do produto 3: '))
-prodt4 =  int(input('Digite o valor do produto 4: '))
-prodt5 =  int(input('Digite o valor do produto 5: '))
+prodt1 =  float(input('Digite o valor do produto 1: '))
+prodt2 =  float(input('Digite o valor do produto 2: '))
+prodt3 =  float(input('Digite o valor do produto 3: '))
+prodt4 =  float(input('Digite o valor do produto 4: '))
+prodt5 =  float(input('Digite o valor do produto 5: '))
 
 total = (prodt1 + prodt2 + prodt3 + prodt4 + prodt5)
 
-print (f'O valor total dos produtos é: R${total}')
+print (f'O valor total dos produtos é: R${total:.2f}')
 
-cliente = int(input('qual o dinheiro que sera aplicado?'))
+cliente = float(input('qual o dinheiro que sera aplicado?'))
 
 troco = (cliente - total)
 
-print (f'o troco é de R${troco}')
+print (f'o troco é de R${troco:.2f}')
--- moeda.py
+++ moeda.py
@@ -1,5 +1,6 @@
 d = int(input('Digite quantos dolares voce tem: '))
+#cotacao do dolar hoje 5.45 então ja fiz automatico para o cliente não precisar decorar a cotação
 
-real = (d * 6)
+real = (d * 5.45)
 
 print(f'Voce tem R${real} ')
