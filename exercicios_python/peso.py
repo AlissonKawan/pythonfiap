5e7b3fe16f247069698fcae6086496055c5997fa
AlissonKawan <rm567598@fiap.com.br>
2025-09-13 23:18:29 -0300

    commit

--- exercicios_python/peso.py
+++ exercicios_python/peso.py
@@ -1,4 +1,4 @@
-alt = int(input("Digite sua altura: "))
+alt = float(input("Digite sua altura: "))
 peso = int(input("Digite seu peso: "))
 
 imc = (peso/alt**2)
@@ -7,5 +7,5 @@ if(imc<20):
     print("Abaixo do peso")
 elif (imc<25):
     print("Voce esta no peso ideal")
-    
-else : ("Voce esta acima do peso")
+
+else : print("Voce esta acima do peso")
